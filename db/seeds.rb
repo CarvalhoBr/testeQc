946e72204d5e90ad8aba4ee6d8e07a30ad6aa73a
brandon <brandonsouza27@gmail.com>
2021-01-11 10:28:28 -0300

    Criando seeds para questões

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+question_list = JSON.parse(File.read('questions.json'))
+question_access_list = JSON.parse(File.read('question_access.json'))
+
+questions = Question.create(question_list)
